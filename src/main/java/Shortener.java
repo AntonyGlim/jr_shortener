b3e410c774259528c823727e077f31391244380d
Antony.Glim <Antony.Glim@gmail.com>
2019-08-07 22:47:09 +0300

    projec created@

---
+++ src/main/java/Shortener.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task33.task3310;
+
+public class Shortener {
+
+    /*будет возвращать идентификатор id для заданной строки.*/
+    public Long getId(String string){
+        return 123456789L;
+    }
+
+    /*будет возвращать строку для заданного идентификатора или null, если передан неверный идентификатор.*/
+    public String getString(Long id){
+        return null;
+    }
+
+}
