805b2fdc35cafd120dd41258956f247f402d42eb
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-08-08 18:34:49 +0300

    3.

--- src/main/java/Shortener.java
+++ src/main/java/Shortener.java
@@ -1,15 +1,31 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 public class Shortener {
 
+
+    private StorageStrategy storageStrategy; //будет храниться стратегия хранения данных
+    private Long lastId = 0L; //Это поле будет отвечать за последнее значение идентификатора, которое было использовано при добавлении новой строки в хранилище.
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
+
     /*будет возвращать идентификатор id для заданной строки.*/
-    public Long getId(String string){
-        return 123456789L;
+    public synchronized Long getId(String string){
+        if (storageStrategy.containsValue(string))
+            return storageStrategy.getKey(string);
+        else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+        }
+        return lastId;
     }
 
     /*будет возвращать строку для заданного идентификатора или null, если передан неверный идентификатор.*/
-    public String getString(Long id){
-        return null;
+    public synchronized String getString(Long id){
+        return storageStrategy.getValue(id);
     }
 
 }
