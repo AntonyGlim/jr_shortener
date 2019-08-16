2dee1a76faaeffc3346991fbae4f3a7d8ab87e91
Antony.Glim <Antony.Glim@gmail.com>
2019-08-16 22:56:18 +0300

    9.

--- src/main/java/strategy/FileBucket.java
+++ src/main/java/strategy/FileBucket.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task33.task3310.strategy;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -18,6 +17,52 @@ public class FileBucket {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    /** должен возвращать размер файла на который указывает path */
+    public long getFileSize(){
+        try {
+            return Files.size(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /** должен сериализовывать переданный entry в файл. Учти, каждый entry может содержать еще один entry. */
+    public void putEntry(Entry entry){
+        try (OutputStream out = Files.newOutputStream(path)) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(out)) {
+                if (entry.next != null) {
+                    putEntry(entry.next);
+                }
+                oos.writeObject(entry);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** должен забирать entry из файла. Если файл имеет нулевой размер, вернуть null */
+    public Entry getEntry(){
+        if (getFileSize() == 0) return null;
+        Entry entry = null;
+        try (InputStream in = Files.newInputStream(path)) {
+            try (ObjectInputStream ois = new ObjectInputStream(in)) {
+                entry = (Entry) ois.readObject();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return entry;
+    }
+
+    /** удалять файл на который указывает path */
+    public void remove(){
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
